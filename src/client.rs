14bb3cc17ac9382ba4eeff6b4e9f07e429dc8e1b
Di Xu <xudifsd@gmail.com>
2016-03-18 00:21:35 +0800

    client almost finished

--- src/client.rs
+++ src/client.rs
@@ -5,7 +5,9 @@ extern crate rustc_serialize as serialize;
 
 use std::str::from_utf8;
 use websocket::client::request::Url;
-use websocket::{Client, Message, Sender, Receiver};
+use websocket::{Client, Message};
+use websocket::Receiver as WebReceiver;
+use websocket::Sender as WebSender;
 use websocket::message::Type;
 use serialize::json;
 
@@ -17,6 +19,10 @@ use std::thread;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
+
+use std::sync::mpsc::{channel, Sender, Receiver};
+
+const CB_READER_SLEEP_MS: u64 = 1000;
 
 fn read_server_ip() -> String {
     let default = "127.0.0.1".to_string();
@@ -37,28 +43,82 @@ fn read_server_ip() -> String {
     }
 }
 
-fn main() {
-    let ip = read_server_ip();
-    let mut ctx = ClipboardContext::new().unwrap();
-    let url = format!("{}{}{}", "ws://", ip, ":31415");
-
+fn web_adaptor(url: String, cb_receiver: Receiver<String>) {
     let ws_uri = Url::parse(&url[..]).unwrap();
     let request = Client::connect(ws_uri).unwrap();
     let response = request.send().unwrap();
 
+    let mut ctx = ClipboardContext::new().unwrap();
+
+    // TODO add retry
     match response.validate() {
         Ok(()) => (),
         Err(e) => {
             println!("{:?}", e);
             return;
         }
-    }
+    };
+    let (mut web_sender, mut web_receiver) = response.begin().split();
 
-    let (mut sender, mut receiver) = response.begin().split();
+    thread::spawn(move || {
+        loop {
+            match cb_receiver.try_recv() {
+                Ok(cb_content) => {
+                    let response = Message::text(cb_content);
+                    web_sender.send_message(&response).unwrap();
+                }
+                _ => ()
+            }
+            let message = web_receiver.recv_message(); // this will block, unless server send ping
+            // TODO retry
+            let message: Message = match message {
+                Ok(message) => message,
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    return;
+                }
+            };
 
+            match message.opcode {
+                Type::Text => {
+                    ctx.set_contents(from_utf8(&*message.payload).unwrap().to_string());
+                }
+                Type::Binary => {
+                    println!("received binary, which is not supported");
+                }
+                Type::Close => {
+                    println!("server closed connection");
+                    return
+                }
+                Type::Ping => {
+                    println!("client received ping");
+                    web_sender.send_message(&Message::ping(message.payload)).unwrap();
+                }
+                _ => (),
+            }
+        }
+    });
+}
+
+fn main() {
+    let ip = read_server_ip();
+    let mut ctx = ClipboardContext::new().unwrap();
+    let url = format!("{}{}{}", "ws://", ip, ":31415");
+
+    let (sender, receiver) = channel();
+
+    web_adaptor(url, receiver);
+
+    let mut ctx = ClipboardContext::new().unwrap();
+    let mut content = "".to_string();
     loop {
-        sender.send_message(&Message::text(ctx.get_contents().unwrap()));
+        let mut cur = ctx.get_contents().unwrap();
 
-        thread::sleep(Duration::from_millis(1000));
+        if content != cur {
+            content = cur;
+            sender.send(content.clone());
+        }
+
+        thread::sleep(Duration::from_millis(CB_READER_SLEEP_MS));
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -5,6 +5,10 @@ use std::str::from_utf8;
 use websocket::{Server, Message, Sender, Receiver};
 use websocket::message::Type;
 
+use std::time::Duration;
+
+const SERVER_SLEEP_MS: u64 = 1000;
+
 fn main() {
     let addr = "0.0.0.0:31415".to_string();
 
@@ -16,6 +20,9 @@ fn main() {
             request.validate().unwrap();
             let response = request.accept();
             let (mut sender, mut receiver) = response.send().unwrap().split();
+
+            let ping = Message::ping(vec![]);
+            sender.send_message(&ping).unwrap();
 
             for message in receiver.incoming_messages() {
                 let message: Message = match message {
@@ -33,14 +40,16 @@ fn main() {
                         println!("{}", from_utf8(&*message.payload).unwrap());
                         sender.send_message(&response).unwrap()
                     },
-                    Type::Binary => sender.send_message(&Message::binary(message.payload)).unwrap(),
+                    Type::Binary => println!("received binary, which is not supported"),
                     Type::Close => {
                         let _ = sender.send_message(&Message::close());
                         return;
                     }
                     Type::Ping => {
-                        let message = Message::pong(message.payload);
+                        println!("server received ping");
+                        let message = Message::ping(message.payload);
                         sender.send_message(&message).unwrap();
+                        thread::sleep(Duration::from_millis(SERVER_SLEEP_MS))
                     }
                     _ => (),
                 }
