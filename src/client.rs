0a40e8751a148bf7c817410e20d330b365afd514
Di Xu <xudifsd@gmail.com>
2016-03-17 17:58:28 +0800

    add server

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,13 @@
 extern crate clipboard;
+extern crate websocket;
+
+extern crate rustc_serialize as serialize;
+
+use std::str::from_utf8;
+use websocket::client::request::Url;
+use websocket::{Client, Message, Sender, Receiver};
+use websocket::message::Type;
+use serialize::json;
 
 use clipboard::ClipboardContext;
 
@@ -6,9 +15,27 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
+    let mut ctx = ClipboardContext::new().unwrap();
+    let url = "ws://127.0.0.1:9001".to_string();
+    let agent = "rust-websocket";
+
+    let ws_uri = Url::parse(&url[..]).unwrap();
+    let request = Client::connect(ws_uri).unwrap();
+    let response = request.send().unwrap();
+
+    match response.validate() {
+        Ok(()) => (),
+        Err(e) => {
+            println!("{:?}", e);
+            return;
+        }
+    }
+
+    let (mut sender, mut receiver) = response.begin().split();
+
     loop {
-        let mut ctx = ClipboardContext::new().unwrap();
-        println!("{}", ctx.get_contents().unwrap());
+        sender.send_message(&Message::text(ctx.get_contents().unwrap()));
+
         thread::sleep(Duration::from_millis(1000));
     }
 }
---
+++ src/server.rs
@@ -0,0 +1,50 @@
+extern crate websocket;
+
+use std::thread;
+use std::str::from_utf8;
+use websocket::{Server, Message, Sender, Receiver};
+use websocket::message::Type;
+
+fn main() {
+    let addr = "127.0.0.1:9001".to_string();
+
+    let server = Server::bind(&addr[..]).unwrap();
+
+    for connection in server {
+        thread::spawn(move || {
+            let request = connection.unwrap().read_request().unwrap();
+            request.validate().unwrap();
+            let response = request.accept();
+            let (mut sender, mut receiver) = response.send().unwrap().split();
+
+            for message in receiver.incoming_messages() {
+                let message: Message = match message {
+                    Ok(message) => message,
+                    Err(e) => {
+                        println!("{:?}", e);
+                        let _ = sender.send_message(&Message::close());
+                        return;
+                    }
+                };
+
+                match message.opcode {
+                    Type::Text => {
+                        let response = Message::text(from_utf8(&*message.payload).unwrap());
+                        println!("{}", from_utf8(&*message.payload).unwrap());
+                        sender.send_message(&response).unwrap()
+                    },
+                    Type::Binary => sender.send_message(&Message::binary(message.payload)).unwrap(),
+                    Type::Close => {
+                        let _ = sender.send_message(&Message::close());
+                        return;
+                    }
+                    Type::Ping => {
+                        let message = Message::pong(message.payload);
+                        sender.send_message(&message).unwrap();
+                    }
+                    _ => (),
+                }
+            }
+        });
+    }
+}
